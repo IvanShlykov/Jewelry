6d3e5f30e1b94963c432ca22715d1607a287693a
IvanShlykov <shivanig@yandex.ru>
2024-05-02 17:43:42 +0300

    init

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -16,3 +16,5 @@ export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
+//ksdjgvisdbck//
